b6faa1177500381b6ff7eed925fe0d95daa43f0a
Zekun Shen <17345356+buszk@users.noreply.github.com>
2021-05-04 22:12:54 +0000

    kill python scripts more precisely

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkill -9 bash || true
-pkill -9 python || true
+# pkill -9 bash || true
+ps aux|grep python|grep drifuzz |awk '{print $2}'|xargs -I '{}' kill -9 '{}' || true
 pkill -9 gdb || true
 pkill -9 panda || true
