75ddbad8c84d89776bb76362ca4658f038143748
buszk <bruceshenzk@gmail.com>
2021-10-07 14:34:23 -0400

    Fix

--- scripts/debug_random.sh
+++ scripts/debug_random.sh
@@ -7,7 +7,7 @@ fi
 
 target=$1
 work=work-$target-random
-seed=seed-random
+seed=seed/seed-random
 
 echo "target: $target"
 echo "work directory: $work"
@@ -16,11 +16,6 @@ echo "seed directory: $seed"
 # Prepare work directory with globalmodule.json
 rm -rf $work
 mkdir -p $work
-
-# Prepare seed directory with initial seed
-rm -rf $seed
-mkdir -p $seed
-cp random_seed $seed/
 
 # Run fuzzing
 python3 fuzzer/drifuzz.py --verbose -D -p 1 $seed $work $target 
