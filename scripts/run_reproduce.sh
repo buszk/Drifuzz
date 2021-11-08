20546786288a0a4bbd07445ada430f021a7fe728
Zekun Shen <bruceshenzk@gmail.com>
2021-11-08 21:13:49 +0000

    update scripts

--- scripts/run_reproduce.sh
+++ scripts/run_reproduce.sh
@@ -4,14 +4,14 @@ target=$1
 work=work/work-reproduce
 
 if [ $# == 1 ]; then
-input=~/Workspace/git/drifuzz-concolic/work/$target/out/0
+input=../drifuzz-concolic/work/$target/out/0
 else
 input=$2
 fi
 
 rm -rf $work
 mkdir -p $work
-cp ~/Workspace/git/drifuzz-concolic/work/$target/$target.sav $work/globalmodule.json
+cp ../drifuzz-concolic/work/$target/$target.sav $work/globalmodule.json
 
 python3 fuzzer/reproduce.py --Purge --reproduce $input seed/seed-random $work $target
 stty sane
