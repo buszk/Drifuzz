fe5cd1daa8bc42a179d91b1f0ab412d20aa4f23c
Zekun Shen <bruceshenzk@gmail.com>
2022-02-09 15:17:20 -0500

    fix script whitespace

--- image/prog-init.sh
+++ image/prog-init.sh
@@ -2,9 +2,9 @@
 
 modprobe $1
 sleep 1
-if ip link | grep "eth0"; then                                                                                                                                                                                                                                                                                                                                                                                                          
-ip link set dev eth0 up                                                                                                                                                                                                                                                                                                                                                                                                                 
-elif ip link | grep "wlan0"; then                                                                                                                                                                                                                                                                                                                                                                                                       
-ip link set dev wlan0 up                                                                                                                                                                                                                                                                                                                                                                                                                
+if ip link | grep "eth0"; then
+ip link set dev eth0 up
+elif ip link | grep "wlan0"; then
+ip link set dev wlan0 up
 fi
 cat /proc/modules
